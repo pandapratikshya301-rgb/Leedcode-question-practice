5d2441ca3c5ee07f159d9bb8cf86eb8c5d48a17a
pandapratikshya301-rgb <pandapratikshya301@gmail.com>
2026-09-16 11:00:49 +0530

    Time: 0 ms (100%), Space: 12.4 MB (21.49%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -7,21 +7,16 @@ class Solution(object):
         """
         stack_s = []
         stack_t = []
-
         for ch in s:
             if ch == '#':
                 if stack_s:
                     stack_s.pop()
             else:
                 stack_s.append(ch)
-
         for ch in t:
             if ch == '#':
                 if stack_t:
                     stack_t.pop()
             else:
                 stack_t.append(ch)
-
         return stack_s == stack_t
-
-        
